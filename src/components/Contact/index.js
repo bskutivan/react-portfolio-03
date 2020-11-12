3515f31938e5ab023ee3ce09e00278390b8b7c49
BrandonKutivan <bskutivan@gmail.com>
2020-11-12 14:55:50 -0800

    adjusted Contact form to include email and github instead of form. kept old code as notes within file for review at a later date. Adjusted style to better reflect responsive design

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,61 +1,59 @@
-import React, { useState } from 'react';
-import { validateEmail } from '../../utils/helpers';
-import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import React from 'react';
+// import { validateEmail } from '../../utils/helpers';
+import { Container } from 'reactstrap';
+import './contact_style.css';
 
 function ContactForm() {
 
-    const [formState, setFormState] = useState({ name: '', email: '', message: ''});
-    const { name, message} = formState;
+    // const [formState, setFormState] = useState({ name: '', email: '', message: ''});
+    // const { name, message} = formState;
 
-    const [errorMessage, setErrorMessage] = useState('');
+    // const [errorMessage, setErrorMessage] = useState('');
 
-    function handleChange(e) {
+    // function handleChange(e) {
 
-        if (e.target.name === 'email') {
-            const isValid = validateEmail(e.target.value);
-            console.log(isValid);
-            if(!isValid) {
-                setErrorMessage('Your email is invalid.');
-            } else {
-                setErrorMessage('');
-            }
-        } else {
-            if(!e.target.value.length) {
-                setErrorMessage(`${e.target.name} is required.`);
-            } else {
-                setErrorMessage('');
-            }
-        }
+    //     if (e.target.name === 'email') {
+    //         const isValid = validateEmail(e.target.value);
+    //         console.log(isValid);
+    //         if(!isValid) {
+    //             setErrorMessage('Your email is invalid.');
+    //         } else {
+    //             setErrorMessage('');
+    //         }
+    //     } else {
+    //         if(!e.target.value.length) {
+    //             setErrorMessage(`${e.target.name} is required.`);
+    //         } else {
+    //             setErrorMessage('');
+    //         }
+    //     }
 
-        if (!errorMessage) {
-            setFormState({...formState, [e.target.name]: e.target.value });
-        }
-    }
+    //     if (!errorMessage) {
+    //         setFormState({...formState, [e.target.name]: e.target.value });
+    //     }
+    // }
 
-    function handleSubmit(e) {
-        e.preventDefault();
-        console.log(formState);
-    }
+    // function handleSubmit(e) {
+    //     e.preventDefault();
+    //     console.log(formState);
+    // }
 
     return(
-        <Form id="contact-form" onSubmit={handleSubmit}>
-            <h1 data-testid="h1tag">Contact me</h1>
-            <FormGroup>
-                <Label for="name">Name:</Label>
-                <Input type="text" name="name" defaultValue={name} onBlur={handleChange} />
-            </FormGroup>
-            <FormGroup>
-                <Label for="message">Message:</Label>
-                <Input type="textarea" name="message" defaultValue={message} onBlur={handleChange} />
-            </FormGroup>
-            {errorMessage && (
-                <div>
-                    <p className="error-text">{errorMessage}</p>
+        <div>
+            <Container class="contactContainer">
+                <div class="flex">
+                <div class="contactDiv">
+                    <h4 class="contactH4">Do you want to build something together? </h4>
+                    <p>Feel free to reach me via email at bskutivan@gmail.com!</p>
                 </div>
-            )}
-            <Button data-testid="subbutton" type="submit">Submit</Button>
-        </Form>
-    
+                <div class="contactDiv">
+                    <h4 class="contactH4">Want to see what else I've built? </h4>
+                    <p>See my github: <a href="https://bit.ly/35rmnHP">Github!</a></p>
+                </div>
+                </div>
+            </Container>
+        </div>
+        
     
     // <section>
     //   <h1 data-testid="h1tag">Contact me</h1>
